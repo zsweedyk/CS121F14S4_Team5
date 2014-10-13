1a31e4275a72073ec434f81fce0e09e95fa01523
Vincent Fiorentini <vincent@panatechcomputer.com>
2014-10-13 09:57:58 -0700

    Initial Commit

---
+++ Pipe Dream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pipe Dream
+//
+//  Created by Vincent Fiorentini on 10/13/14.
+//  Copyright (c) 2014 Flapjack Stack Hack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
