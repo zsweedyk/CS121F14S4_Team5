167b64be598e2f0bd3744f7db5c58ab61ce78dff
Vincent Fiorentini <vincent@panatechcomputer.com>
2014-10-13 16:12:49 -0700

    Initial Commit

---
+++ PipeDreamTests/PipeDreamTests.m
@@ -0,0 +1,40 @@
+//
+//  PipeDreamTests.m
+//  PipeDreamTests
+//
+//  Created by Vincent Fiorentini on 10/13/14.
+//  Copyright (c) 2014 Flapjack Stack Hack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PipeDreamTests : XCTestCase
+
+@end
+
+@implementation PipeDreamTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
