9dc125fa25847fd317cf2a8c7519661d0132f0b3
jeansung <jsung@hmc.edu>
2014-10-20 15:13:06 -0700

    Changes from feature-cellview before pulling from local develop

--- PipeDream/PDCellModel.m
+++ PipeDream/PDCellModel.m
@@ -12,13 +12,43 @@
 
 #pragma mark Public methods
 
-
+/*
+ * Rotates the openings of this CellModel clockwise.
+ */
 - (void) rotateClockwise {
     BOOL tempIsOpenWest = [_openings isOpenWest];
     [_openings setIsOpenWest: [_openings isOpenSouth]];
     [_openings setIsOpenSouth: [_openings isOpenEast]];
     [_openings setIsOpenEast: [_openings isOpenNorth]];
     [_openings setIsOpenNorth: tempIsOpenWest];
+}
+
+/*
+ * YES if this CellModel's opening is open to the north.
+ */
+- (BOOL) isOpenNorth {
+    return [_openings isOpenNorth];
+}
+
+/*
+ * YES if this CellModel's opening is open to the east.
+ */
+- (BOOL) isOpenEast {
+    return [_openings isOpenEast];
+}
+
+/*
+ * YES if this CellModel's opening is open to the south.
+ */
+- (BOOL) isOpenSouth {
+    return [_openings isOpenSouth];
+}
+
+/*
+ * YES if this CellModel's opening is open to the west.
+ */
+- (BOOL) isOpenWest {
+    return [_openings isOpenWest];
 }
 
 #pragma mark Private methods
