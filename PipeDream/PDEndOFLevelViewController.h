c9061c12b45e61ec08aaca45e780b35dc885f097
paulayuan <pyuan@hmc.edu>
2014-11-16 12:39:03 -0800

    Added EndOfLevelViewContrloller class and the dialog window in the storyboard.

---
+++ PipeDream/PDEndOFLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDEndOFLevelViewController.h
+//  PipeDream
+//
+//  Created by CS121 on 11/14/14.
+//  Copyright (c) 2014 Flapjack Stack Hack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDEndOFLevelViewController : UIViewController
+
+@end
